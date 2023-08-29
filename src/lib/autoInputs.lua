a6b006d6d1020499041c2702d597a3df132b018c
Patrick Hund <patrick.hund@adevinta.com>
2023-08-29 11:49:40 +0200

    Added support for keyboard, pitch bend wheel, modulation wheel, sustain pedal, channel pressure and expression pedal to config “from-scratch”

---
+++ src/lib/autoInputs.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        pattern = "<100x>? yy zz",
+        name = "keyboard",
+        port = 2
+    },
+    {
+        pattern = "E? xx yy",
+        name = "pitchBend",
+        value = "y*128 + x",
+        port = 2
+    },
+    {
+        pattern = "B? 01 xx",
+        name = "modulation",
+        port = 2
+    },
+    {
+        pattern = "B? 40 xx",
+        name = "sustain",
+        port = 2
+    },
+    {
+        pattern = "D? xx ??",
+        name = "channelPressure",
+        port = 2
+    },
+    {
+        pattern = "B? 0B xx",
+        name = "expression",
+        port = 2
+    }
+}
