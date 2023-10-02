f148f92d91274c3e491ab0d7ba17d5d586e8252a
Patrick Hund <wiekatz@gmail.com>
2023-10-02 13:13:36 +0200

    Added deviceType to all devices in mapping

--- src/setState/deviceType.lua
+++ src/setState/deviceType.lua
@@ -1,11 +1,12 @@
 local stateUtils = require("src.lib.stateUtils")
 local items = require("src.lib.items")
-local debugUtils = require("src.lib.debugUtils")
+local log = require("src.lib.debugUtils").log
 
 return function(changedItems)
     for _, changedItemIndex in ipairs(changedItems) do
         if changedItemIndex == items.deviceType.index then
-            local deviceType = remote.get_item_text_value(changedItemIndex);
+            local deviceType = remote.get_item_text_value(changedItemIndex)
+            log("device type: " .. deviceType)
             stateUtils.set("deviceType", deviceType)
             return
         end
